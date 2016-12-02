f166f598f142aa45ce410f0281061d8a787af845
Richard Remer <reanjr@gmail.com>
2016-12-01 23:38:25 -0800

    bind lead and rest arguments

--- qwcli.js
+++ qwcli.js
@@ -1,2 +1,8 @@
 module.exports = require("./lib/cli");
+module.exports.head = require("./lib/sentinel").head;
+module.exports.lead = require("./lib/sentinel").lead;
+module.exports.rest = require("./lib/sentinel").rest;
+module.exports.State = require("./lib/state");
+module.exports.ParseError = require("./lib/parse-error");
 module.exports.Interface = require("./lib/interface");
+
