1d21dac749054c36ef0d2bb108b2538fdd97f3e0
Richard Remer <rich@zingle.me>
2016-12-02 11:46:50 -0800

    create option terminator binding in default CLI

--- qwcli.js
+++ qwcli.js
@@ -2,7 +2,9 @@ module.exports = require("./lib/cli");
 module.exports.head = require("./lib/sentinel").head;
 module.exports.lead = require("./lib/sentinel").lead;
 module.exports.rest = require("./lib/sentinel").rest;
+module.exports.term = require("./lib/sentinel").term;
+module.exports.stateful = require("./lib/sentinel").stateful;
 module.exports.State = require("./lib/state");
 module.exports.ParseError = require("./lib/parse-error");
 module.exports.Interface = require("./lib/interface");
-
+module.exports.handler = require("./lib/handler");
